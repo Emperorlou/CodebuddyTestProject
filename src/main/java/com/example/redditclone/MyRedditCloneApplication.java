05b0885abcfdb7de4a55de521d5d58b2ea05eadd
Nik <nikolasarmstrong@gmail.com>
2023-08-22 17:24:50 -0400

    Added springboot

---
+++ src/main/java/com/example/redditclone/MyRedditCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRedditCloneApplication.class, args);
+	}
+
+}
